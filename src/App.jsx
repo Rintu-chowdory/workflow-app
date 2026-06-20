0f4fd946fce2df9ee42fe5d06ca7a2b77f743c49
Rintu-chowdory <chowdoryrintu492@gmail.com>
2026-06-20 13:30:58 +0200

    Add src/App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout'
+import Dashboard from './pages/Dashboard'
+import Analytics from './pages/Analytics'
+import Tasks from './pages/Tasks'
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Dashboard />} />
+        <Route path="analytics" element={<Analytics />} />
+        <Route path="tasks" element={<Tasks />} />
+      </Route>
+    </Routes>
+  )
+}
